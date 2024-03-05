6974e1d7f2a22140d555f6516195b65becbdac54
the-aphrodite <128906057+the-aphrodite@users.noreply.github.com>
2024-03-05 23:31:33 +0200

    Update home.dart

--- lib/Pages/pagesUser/BNavBarPages/home.dart
+++ lib/Pages/pagesUser/BNavBarPages/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import 'package:grad_proj/Pages/pagesUser/BNavBarPages/workerslist.dart';
-import 'package:grad_proj/Pages/pagesUser/reqCategory.dart';
+import 'package:the_proj_on_github/Pages/pagesUser/BNavBarPages/workerslist.dart';
+
+import '../../../Domain/customAppBar.dart';
+import '../../menu.dart';
+import '../reqCategory.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,6 +15,8 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   List<String> catNames = [
     'Carpenters',
     'Plumbers',
@@ -37,101 +42,24 @@ class HomeState extends State<Home> {
   ];
 
   List<String> catImages = [
-    //   "assets/images/categories/carpenter.jpg",
-    // "assets/images/man.jpg",
-    // "assets/images/categories/electricians.jpg",
-    // "assets/images/categories/painter.jpg",
-    // "assets/images/categories/tiler.jpeg",
-    // "assets/images/categories/Plastering.jpeg",
-    // "assets/images/categories/Appliance Repair Technician.jpeg",
-    // "assets/images/categories/Alumetal Technicians.jpeg",
-    // "assets/images/categories/Marble Craftsmen.jpg",
-    "assets/images/profile.png",
-    "assets/images/profile.png",
-    "assets/images/profile.png",
-    "assets/images/profile.png",
-    "assets/images/profile.png",
-    "assets/images/profile.png",
-    "assets/images/profile.png",
-    "assets/images/profile.png",
-    "assets/images/profile.png",
+    "assets/images/categories/carpenter.jpg",
+    "assets/images/categories/plumper.jpg",
+    "assets/images/categories/electricians.jpg",
+    "assets/images/categories/painter.jpg",
+    "assets/images/categories/tiler.jpg",
+    "assets/images/categories/Plastering.jpg",
+    "assets/images/categories/Appliance Repair Technician.jpg",
+    "assets/images/categories/Alumetal Technicians.jpg",
+    "assets/images/categories/Marble Craftsmen.jpg",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      appBar: CustomAppBar(scaffoldKey: _scaffoldKey,showSearchBox: true,),
       body: ListView(
         children: [
-          Container(
-            padding: EdgeInsets.zero,
-            decoration: const BoxDecoration(
-              color: Color(0xFFBBA2BF),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  left: 0,
-                  child: SvgPicture.asset(
-                    "assets/images/Rec that Contain menu icon &profile.svg",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  top: 13,
-                  child: IconButton(
-                    onPressed: () {
-                      // Navigator.pushNamed(context, "/signup");
-                    },
-                    icon: const Icon(
-                      Icons.menu,
-                      color: Colors.white,
-                      size: 40,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 15,
-                  left: 0,
-                  right: 0,
-                  child: Center(
-                    child: SvgPicture.asset("assets/images/MR. House.svg"),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 90.0, right: 30, left: 30, bottom: 15),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: "Search technican name",
-                      contentPadding: EdgeInsets.zero,
-                      prefixIcon: const Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: const BorderSide(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const Positioned(
-                  right: 15,
-                  top: 15,
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundImage: AssetImage('assets/images/profile.png'),
-                  ),
-                ),
-              ],
-            ),
-          ),
           Padding(
             padding:
                 const EdgeInsets.only(top: 20, left: 15, right: 15, bottom: 20),
@@ -211,6 +139,7 @@ class HomeState extends State<Home> {
         child: const Icon(Icons.add_chart_rounded),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      drawer: Menu(scaffoldKey: _scaffoldKey,),
     );
   }
 }
