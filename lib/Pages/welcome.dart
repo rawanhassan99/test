ba58aaa0ca965dc58b675c3d3dbd8d8b3d3bb92a
alyaa149 <133502822+alyaa149@users.noreply.github.com>
2024-03-05 04:21:42 +0200

    Update welcome.dart

--- lib/Pages/welcome.dart
+++ lib/Pages/welcome.dart
@@ -1,52 +1,96 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class welcome extends StatelessWidget {
-  const welcome({super.key});
+import 'package:flutter/material.dart';
+import 'package:grad_proj/Pages/pagesUser/login.dart';
+import 'package:grad_proj/Pages/pagesWorker/login.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Choose User or Worker'),
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.grey[200],
-          padding: const EdgeInsets.all(200),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  // Handle user 1 selection
-                },
-                child: Column(
-                  children: [
-                    Image.asset(
-                      'assets/images/userlogo.png', // Path to user 1 logo image
-                      height: 30,
-                    ),
-                    const Text('User '),
-                  ],
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: [
+            Center(
+              child: Container(
+                width: 320,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF5F3F3),
+                  borderRadius: BorderRadius.circular(30.0),
+                  border: Border.all(
+                    color: Colors.black26,
+                    width: 2,
+                  ),
+                ),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 100,
+                        height: 120,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Login()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFFBBA2BF),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                'assets/images/userlogo.png', // Path to user 1 logo image
+                                height: 50,
+                              ),
+                              const Text('User'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      SizedBox(
+                        width: 100,
+                        height: 120,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginWorker()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFFBBA2BF),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                'assets/images/workerlogo.png', // Path to user 2 logo image
+                                height: 50,
+                              ),
+                              const Text('Worker'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(width: 50),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle user 2 selection
-                },
-                child: Column(
-                  children: [
-                    Image.asset(
-                      'assets/images/workerlogo.png', // Path to user 2 logo image
-                      height: 50,
-                    ),
-                    const Text('Worker'),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
